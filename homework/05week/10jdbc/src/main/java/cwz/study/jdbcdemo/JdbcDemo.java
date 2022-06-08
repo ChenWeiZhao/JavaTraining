db9acf5265250763039e96460c7093ed4ade2547
chenwz <419168526@qq.com>
2022-06-08 20:43:30 +0800

    feat：week05.

--- homework/05week/10jdbc/src/main/java/cwz/study/jdbcdemo/JdbcDemo.java
+++ homework/05week/10jdbc/src/main/java/cwz/study/jdbcdemo/JdbcDemo.java
@@ -1,9 +1,9 @@
 package cwz.study.jdbcdemo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.*;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class JdbcDemo {
@@ -17,6 +17,47 @@ public class JdbcDemo {
         //0.加载驱动
         Class.forName(DB_DRIVER);
         //1.使用 PreparedStatement 执行数据库操作，手动提交
+        //jdbcPreparedStatement();
+
+        // 2.批处理方式
+        jdbcBatch();
+
+    }
+
+    private static void jdbcBatch() throws SQLException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            // 3、开启手动提交
+            connection.setAutoCommit(false);
+            String sql = "insert into city(city_id,city,country_id) value(?,?,?)";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                for (int i = 0; i < 100; i++) {
+                    int initId = 1000 + i;
+                    preparedStatement.setInt(1, initId);
+                    preparedStatement.setString(2, "cwz" + i);
+                    preparedStatement.setInt(3, initId);
+                    preparedStatement.addBatch();
+                }
+                int[] rowCount = preparedStatement.executeBatch();
+                System.out.println("插入行数" + Arrays.stream(rowCount).sum());
+                connection.commit();
+            }
+        } catch (Exception e) {
+            // 出现异常回滚
+            if (connection != null) {
+                connection.rollback();
+            }
+            throw e;
+        } finally {
+            // 关闭连接
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    private static void jdbcPreparedStatement() throws SQLException {
         //创建连接
         Connection connection = null;
         try {
@@ -41,7 +82,6 @@ public class JdbcDemo {
                 connection.close();
             }
         }
-
     }
 
 }
